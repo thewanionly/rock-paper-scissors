b38a25bb35fbf0111bf3e1d70111077e907637d3
thewanionly <pelloani@gmail.com>
2023-04-06 22:35:34 +0800

    chore: install styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import type { ReactElement } from 'react'
+import styled from 'styled-components'
+
+const S = {
+  App: styled.main`
+    display: flex;
+    justify-content: center;
+  `,
+}
 
 const App = (): ReactElement => {
   return (
-    <main>
+    <S.App>
       <h1>Rock Paper Scissors</h1>
-    </main>
+    </S.App>
   )
 }
 
