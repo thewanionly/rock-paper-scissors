977bf0934e0bbbc10bbd0dc3f47a114f025582d2
thewanionly <pelloani@gmail.com>
2023-04-30 22:57:39 +0800

    refactor: update animation of switch component

--- src/components/Switch/Switch.tsx
+++ src/components/Switch/Switch.tsx
@@ -1,36 +1,33 @@
 import { ChangeEvent, useState } from 'react'
+import { motion } from 'framer-motion'
 import styled from 'styled-components'
 
-const StyledSwitch = styled.label<SwitchStylesProps>`
-  cursor: pointer;
-  text-indent: -9999px;
-  width: 5.5rem;
-  height: 3rem;
-  background: ${({ checked, theme }) =>
-    checked ? theme.colors.switchOnBg : theme.colors.switchOffBg};
-  display: block;
-  border-radius: 100px;
-  position: relative;
-
-  &:after {
-    content: '';
-    position: absolute;
-    left: ${({ checked }) => (checked ? '50%' : '5px')};
-    top: 3px;
-    width: 2.4rem;
-    height: 2.4rem;
-    background: ${({ theme }) => theme.colors.switchCircleBg};
-    border-radius: 90px;
-    transition: 0.3s;
-  }
-`
-
 const S = {
+  Switch: styled.label<SwitchStylesProps>`
+    cursor: pointer;
+    text-indent: -9999px;
+    width: 5.5rem;
+    height: 3rem;
+    background: ${({ checked, theme }) =>
+      checked ? theme.colors.switchOnBg : theme.colors.switchOffBg};
+    display: flex;
+    justify-content: ${({ checked }) => (checked ? 'flex-end' : 'flex-start')};
+    align-items: center;
+    padding: 0.5rem;
+    border-radius: 100px;
+    position: relative;
+  `,
   SwitchCheckbox: styled.input`
     position: absolute;
     /* hide native radio input while still keeping it technically accessible */
     appearance: none;
     margin: 0;
+  `,
+  SwitchHandle: styled(motion.div)`
+    width: 2.4rem;
+    height: 2.4rem;
+    background: ${({ theme }) => theme.colors.switchCircleBg};
+    border-radius: 90px;
   `,
 }
 
@@ -54,7 +51,7 @@ export const Switch = ({ id, className = '', checked = false, onChange }: Switch
   }
 
   return (
-    <StyledSwitch
+    <S.Switch
       htmlFor="checkbox"
       checked={isTicked}
       className={className}
@@ -67,6 +64,14 @@ export const Switch = ({ id, className = '', checked = false, onChange }: Switch
         checked={isTicked}
         onChange={handleChange}
       />
-    </StyledSwitch>
+      <S.SwitchHandle
+        layout
+        transition={{
+          type: 'spring',
+          stiffness: 650,
+          damping: 30,
+        }}
+      />
+    </S.Switch>
   )
 }
